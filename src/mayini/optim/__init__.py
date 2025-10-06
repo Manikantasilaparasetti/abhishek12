14885523423647c716d2a053096e10728ad4b94b
907-bot-collab <vc095713@gmail.com>
2025-10-06 23:11:08 +0530

    Update __init__.py

--- src/mayini/optim/__init__.py
+++ src/mayini/optim/__init__.py
@@ -1,7 +1,17 @@
-"""
-Optimizers package.
+# Create corrected optim/__init__.py
+corrected_optim_init = '''"""
+Optimization algorithms for MAYINI Deep Learning Framework.
 """
 
 from .optimizers import Optimizer, SGD, Adam, AdamW, RMSprop
+from .optimizers import StepLR, ExponentialLR, CosineAnnealingLR
 
-__all__ = ['Optimizer', 'SGD', 'Adam', 'AdamW', 'RMSprop']
+__all__ = [
+    'Optimizer',
+    'SGD', 'Adam', 'AdamW', 'RMSprop',
+    'StepLR', 'ExponentialLR', 'CosineAnnealingLR'
+]
+'''
+
+print("✅ CORRECTED optim/__init__.py created")
+print(f"Length: {len(corrected_optim_init)} characters")
